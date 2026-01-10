6d68416d19bed38ca867757014be195dbdd2cd54
bhagyasandakelum <2022csc030@univ.jfn.ac.lk>
2026-01-11 01:14:02 +0530

    edit header and footer components

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const categories = [
@@ -14,7 +13,6 @@ const categories = [
 ];
 
 export default function Header() {
-  const router = useRouter();
   const [query, setQuery] = useState("");
 
   const handleSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
